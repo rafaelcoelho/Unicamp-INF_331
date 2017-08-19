61280764a1bcb7586099351e183382df9aa15074
Rafael Coelho <rafael.asilvacoelho@gmail.com>
2017-08-19 15:24:28 -0300

    Exercise number 1 is done and 2 on going

---
+++ class_19_08_2017/mda20162/generated/Bolsa.java
@@ -0,0 +1,42 @@
+
+/**
+ * The documentation of the class Bolsa
+ * 
+ * @author Acceleo 3 Example - UML to Java generator
+ * @generated
+ */
+
+public class Bolsa {
+   
+   /**
+    * The documentation of the constructor.
+    * 
+    * @generated
+    */
+   public Bolsa() {
+      super();
+   }
+   /**
+    * The documentation of the method start.
+    * 
+    * 
+    * @generated
+    */
+   public void start() {
+      // Start of user code for the body of start
+      // TODO should be implemented
+      // End of user code
+   }
+   
+   /**
+    * The documentation of the method stop.
+    * 
+    * 
+    * @generated
+    */
+   public void stop() {
+      // Start of user code for the body of stop
+      // TODO should be implemented
+      // End of user code
+   }
+}
---
+++ class_19_08_2017/mda20162/generated/Livro.java
@@ -0,0 +1,30 @@
+
+/**
+ * The documentation of the class Livro
+ * 
+ * @author Acceleo 3 Example - UML to Java generator
+ * @generated
+ */
+
+public class Livro {
+   
+   /**
+    * The documentation of the constructor.
+    * 
+    * @generated
+    */
+   public Livro() {
+      super();
+   }
+   /**
+    * The documentation of the method test.
+    * 
+    * 
+    * @generated
+    */
+   public void test() {
+      // Start of user code for the body of test
+      // TODO should be implemented
+      // End of user code
+   }
+}
